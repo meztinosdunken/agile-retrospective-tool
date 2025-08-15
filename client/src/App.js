4f248ca39f1812cf4ce0f7a24725a9d93384fb14
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-14 23:30:15 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Agile Retrospective Tool</h1>
+      <p>Welcome to the Agile Retrospective Tool!</p>
+    </div>
+  );
+};
+
+export default App;
